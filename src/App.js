3fc8b5d38e0670da9197a835eba9a970d60f785b
Diwash <diwashbhattarai86@gmail.com>
2023-01-14 17:48:32 +0545

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './css/App.css';
+import Navbar from './components/Navbar';
+import Card from './components/Card';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Card />
+    </div>
+  );
+}
+
+export default App;
