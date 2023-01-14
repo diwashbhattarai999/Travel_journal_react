3872b9f9bed741234a93c4a0570ce459fcbc3fca
Diwash <diwashbhattarai86@gmail.com>
2023-01-14 21:13:15 +0545

    final commit

---
+++ src/data.js
@@ -0,0 +1,64 @@
+const data = [
+    {
+        id: 0,
+        title: "Mount Everest",
+        location: "Nepal",
+        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+        startDate: "12 Jan, 2023",
+        endDate: "24 Jan, 2023",
+        description: "Mount Everest is Earth's highest mountain above sea level, located in the Mahalangur Himal sub-range of the Himalayas. The China–Nepal border runs across its summit point.Its elevation (snow height) of 8,848.86 m (29,031.7 ft) was most recently established in 2020 by the Chinese and Nepali authorities.",
+        imageUrl: "https://source.unsplash.com/5uwemwi3vac"
+    },
+    {
+        id: 1,
+        title: "Mount Fuji",
+        location: "Japan",
+        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+        startDate: "12 Jan, 2023",
+        endDate: "24 Jan, 2023",
+        description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+        imageUrl: "https://source.unsplash.com/WLxQvbMyfas"
+    },
+    {
+        id: 2,
+        title: "Taj Mahal",
+        location: "India",
+        googleMapsUrl: "https://goo.gl/maps/DrQKBwdbfsGTMX1TA",
+        startDate: "22 Jul, 2023",
+        endDate: "3 Aug, 2023",
+        description: "The Taj Mahal is an Islamic ivory-white marble mausoleum on the right bank of the river Yamuna in the Indian city of Agra. It was commissioned in 1631 by the Mughal emperor Shah Jahan to house the tomb of his favourite wife, Mumtaz Mahal; it also houses the tomb of Shah Jahan himself. ",
+        imageUrl: "https://source.unsplash.com/p4-d9whI0Ts"
+    },
+    {
+        id: 3,
+        title: "Sydney Opera House",
+        location: "Australia",
+        googleMapsUrl: "https://goo.gl/maps/XNAzMjFMJbq9vYnm9",
+        startDate: "27 May, 2023",
+        endDate: "8 Jun, 2023",
+        description: "The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings",
+        imageUrl: "https://source.unsplash.com/JmuyB_LibRo"
+    },
+    {
+        id: 4,
+        title: "Geirangerfjord",
+        location: "Norway",
+        googleMapsUrl: "https://goo.gl/maps/H9rr2m6qj78imyK48",
+        startDate: "01 Oct, 2023",
+        endDate: "18 Nov, 2023",
+        description: "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality.",
+        imageUrl: "https://source.unsplash.com/3PeSjpLVtLg"
+    },
+    {
+        id: 5,
+        title: "Eiffel Tower",
+        location: "France",
+        googleMapsUrl: "https://goo.gl/maps/P7FNJCgtLxHHG1fg8",
+        startDate: "7 Nov, 2023",
+        endDate: "21 Nov, 2023",
+        description: "The Eiffel Tower is a wrought-iron lattice tower on the Champ de Mars in Paris, France. It is named after the engineer Gustave Eiffel, whose company designed and built the tower. Locally nicknamed 'La dame de fer', it was constructed from 1887 to 1889 as the centerpiece of the 1889 World's Fair. ",
+        imageUrl: "https://source.unsplash.com/UzagqG756OU"
+    }
+]
+
+export default data
